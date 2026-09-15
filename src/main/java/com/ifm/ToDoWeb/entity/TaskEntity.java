683f7c26a7784db44ba5182dbcfda0689c913081
derudaiu <iulian-antonio.rudacevschi@ifm.com>
2026-09-15 10:59:24 +0200

    add database support

--- src/main/java/com/ifm/ToDoWeb/entity/TaskEntity.java
+++ src/main/java/com/ifm/ToDoWeb/entity/TaskEntity.java
@@ -1,19 +1,18 @@
 package com.ifm.ToDoWeb.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter @NoArgsConstructor
+@Table(name = "Task")
 public class TaskEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "taskName", nullable = false)
     private String task;
 }
